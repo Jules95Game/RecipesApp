a0ce347334231ef7ae476f480f68c15fe5f21168
Jules95Game <romp.julian1@gmail.com>
2024-05-21 12:07:19 +0200

    Added Recipe controller endpoint

--- recipes-app-spring/src/main/java/org/julian/recipesappspring/recipe/RecipeController.java
+++ recipes-app-spring/src/main/java/org/julian/recipesappspring/recipe/RecipeController.java
@@ -1,6 +1,8 @@
 package org.julian.recipesappspring.recipe;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecipeController {
 
     private final RecipeRepository recipeRepository;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> getHello() {
+        return ResponseEntity.ok("hello recipes!");
+    }
 }
--- recipes-app-spring/src/main/java/org/julian/recipesappspring/recipe/RecipeRepository.java
+++ recipes-app-spring/src/main/java/org/julian/recipesappspring/recipe/RecipeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface RecipeRepository extends JpaRepository<UUID, Recipe> {
+public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
 }
